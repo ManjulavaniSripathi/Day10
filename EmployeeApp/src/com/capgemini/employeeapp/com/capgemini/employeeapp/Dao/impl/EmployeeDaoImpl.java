e7031e5ae9f6983355a7244e83c27a04c9a94aad
ManjulavaniSripathi <42469957+ManjulavaniSripathi@users.noreply.github.com>
2018-09-08 02:22:24 -1100

    Add files via upload

---
+++ EmployeeApp/src/com/capgemini/employeeapp/com/capgemini/employeeapp/Dao/impl/EmployeeDaoImpl.java
@@ -0,0 +1,59 @@
+package com.capgemini.employeeapp.Dao.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.capgemini.employeeapp.Dao.EmployeeDao;
+import com.capgemini.employeeapp.model.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao{
+	private List<Employee>employees=new ArrayList<>();
+
+	@Override
+	public List<Employee> findAllEmployees() {
+		return employees;
+	}
+
+	@Override
+	public Employee findEmployeeById(int employeeId) {
+		for (Employee employee : employees) {
+			if(employee.getEmployeeId()==employeeId)
+			{
+				return employee;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteEmployee(int employeeId) {
+		for (Employee employee : employees) {
+			if(employee.getEmployeeId()==employeeId)
+			{
+				employees.remove(employee);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean addEmployee(Employee employee) {
+		return employees.add(employee);
+	}
+	
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		for (Employee employeeIter : employees) {
+			if(employeeIter.getEmployeeId()==employee.getEmployeeId())
+					{
+						employeeIter.setEmployeeDepartment(employee.getEmployeeDepartment());
+						employeeIter.setEmployeeName(employee.getEmployeeName());
+						employeeIter.setEmployeeSalary(employee.getEmployeeSalary());
+					}
+		}
+		return null;
+	}
+
+}
---
+++ EmployeeApp/src/com/capgemini/employeeapp/com/capgemini/employeeapp/controller/AddEmployeeController.java
@@ -0,0 +1,86 @@
+package com.capgemini.employeeapp.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.capgemini.employeeapp.Dao.EmployeeDao;
+import com.capgemini.employeeapp.Dao.impl.EmployeeDaoImpl;
+import com.capgemini.employeeapp.model.Employee;
+
+
+@WebServlet("/addEmployee")
+public class AddEmployeeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private EmployeeDao employeeDao;
+	private ServletContext context;
+ 
+       
+	   public AddEmployeeController() {
+	        super();
+	employeeDao = new EmployeeDaoImpl(); 
+}
+
+@Override
+public void init(ServletConfig config) throws ServletException {
+	context = config.getServletContext();
+}
+
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+			int empId=Integer.parseInt(request.getParameter("empId"));
+			String empName = request.getParameter("empName");
+			double empSalary=Double.parseDouble(request.getParameter("empSalary"));
+			String empDept= request.getParameter("empDepartment");
+			
+			context.setAttribute("employeeDao", employeeDao);
+			RequestDispatcher dispatcher = null;
+
+			
+			Employee employee = new Employee(empId, empName, empSalary, empDept);
+			
+			if(employeeDao.addEmployee(employee))
+			{
+				 response.sendRedirect("getAllEmployees");
+			}
+			else
+			{
+				dispatcher = request.getRequestDispatcher("error.jsp");
+				dispatcher.forward(request, response);
+				
+			}
+}
+	
+	/*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		int empId=Integer.parseInt(request.getParameter("empId"));
+		String empName=request.getParameter("empName");
+		double empSalary=Double.parseDouble(request.getParameter("empSalary"));
+		String empDept=request.getParameter("empDept");
+		context.setAttribute("employeeDao", employeeDao);
+		Employee employee = new Employee(empId,empName,empSalary,empDept);
+		RequestDispatcher dispatcher = null;
+		if(employeeDao.addEmployee(employee)) {
+			
+			 response.sendRedirect("getAllEmployees");
+			
+		}
+		else {
+			dispatcher = request.getRequestDispatcher("error.jsp");
+			dispatcher.forward(request, response);
+			response.sendRedirect("error.jsp");
+			
+		}
+		
+		
+	}*/
+
+}
